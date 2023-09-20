7bfbb95a2ad8deca57fa42ee945a7892a496f1fd
amgaikwa110905 <amgaikwa110905@gmail.com>
2023-09-20 08:28:36 +0530

    Initial Commit
    Initial commit and assignment

---
+++ 01_VariableAssign.js
@@ -0,0 +1,32 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "Amol Gaikwad";
+console.log(myFullName);
+
+myFullName = "Mr. Amol Bhagwat Gaikwad";
+console.log(myFullName);
+
+
+var test = "I like JS";
+console.log(test);
+
+
+var firstName = "Amol";
+console.log(firstName);
+
+var lastName = "Gaikwad";
+console.log(lastName);
+
+ var age = "30";
+ console.log(age);
+
+ var gender = "male";
+ console.log(gender);
+
+ var address = "Pune";
+ console.log(address);
+
+ var pincode = "123456";
+ console.log(pincode);
+
---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+
+var firstName; //variable declaration
+
+firstName = "Amol"; // variable initialization
+
+console.log(firstName);
+var lastName = "Gaikwad";//variable declaration and initialization
+console.log(lastName);
+
+firstName = "Mr. Amol"; // update
+console.log(firstName);
+
+var test;
+console.log(test);
+
+
