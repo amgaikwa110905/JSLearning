8d9537fb231a9d8b22dc1b438427b226cd1f5c89
amgaikwa110905 <amgaikwa110905@gmail.com>
2023-09-21 11:45:35 +0530

    Create 02_variableBasicAssign.js

---
+++ 02_variableBasicAssign.js
@@ -0,0 +1,16 @@
+
+
+var city = "Pune";
+console.log("City :", city);
+
+var pinCode = 12345;
+console.log("PinCode:", pinCode);
+
+var isCapital = true;
+console.log("Pune is SubCapital city:", isCapital);
+
+var undefined;
+console.log("DataType:", typeof undefined, "value:",undefined);
+
+var stringEmpty = null;
+console.log("DataType of stringEmpty:", typeof stringEmpty, "Value:",stringEmpty);
